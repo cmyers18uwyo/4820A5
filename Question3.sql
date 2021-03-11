aa650b579790a6f089341599c62caa87ca8738d9
Charles Myers <55416416+cmyers18uwyo@users.noreply.github.com>
2021-03-10 19:52:46 -0700

    Add files via upload

--- Question3.sql
+++ Question3.sql
@@ -1,3 +1,9 @@
-\COPY authors (id, author_name)
-FROM 'midterm.authors.q3.csv'
-WITH (FORMAT csv);
+\COPY authors FROM 'midterm.authors.q3.csv' DELIMITER ',' CSV HEADER;
+\COPY books FROM 'midterm.books.q3.csv' DELIMITER ',' CSV HEADER;
+
+
+SELECT
+	* FROM authors
+	FULL OUTER JOIN on (authors.id = books.author_id);
+
+;
